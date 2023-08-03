fad8a619a10e3ab6605299e73e42725df1474635
Jan Kühn <yotka@posteo.org>
2023-08-03 12:09:44 +0200

    Update script

--- src/berlin_orthophotos.py
+++ src/berlin_orthophotos.py
@@ -1,50 +1,70 @@
-# Add the parent directory to the path to make imports work
+"""
+Script to segment orthophotos using a trained model.
+
+Make sure to run export SM_FRAMEWORK=tf.keras before running this script.
+"""
+
 import os
 import sys
-
-module_path = os.path.abspath(os.path.join("../src"))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
 from pathlib import Path
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
 import segmentation_models as sm
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
+# Add the parent directory to the path to make imports work
+module_path = os.path.abspath(os.path.join("../src"))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
 import plot
 import prepare_data
 from smooth_tiled_predictions import predict_img_with_smooth_windowing
 
 # Directory paths
-dir_data_root = Path("../data/orthophotos/")
+dir_data_root = Path().cwd() / "data/orthophotos/"
 dir_input_files = Path(dir_data_root, "input")
 dir_output = Path(dir_data_root, "output")
 dir_results = Path(dir_data_root, "predict", "results")
-dir_models = Path("../models")
 
+# Make sure the directories exist
+for dir_name in [dir_data_root, dir_input_files, dir_output, dir_results]:
+    Path(dir_name).mkdir(parents=True, exist_ok=True)
+
+# Define model to use
+dir_models = Path().cwd() / "data/models"
 file_model = Path(
-    dir_models, f"landcover_25_epochs_resnet34_backbone_batch16_iou_0.76.hdf5"
+    dir_models, f"landcover_25_epochs_resnet50_backbone_batch16_freeze_iou_0.86.hdf5"
 )
 
+# Define classes and count them
 classes = {0: "Not classified", 1: "Building", 2: "Woodland", 3: "Water", 4: "Roads"}
+n_classes = len(classes)
 
+# Define preprocessing function
 BACKBONE = "resnet34"
 preprocess_input = sm.get_preprocessing(BACKBONE)
+scaler = MinMaxScaler()
 
 # Size of patches
 patch_size = 256
 
-# Number of classes
-n_classes = len(classes)
-
-scaler = MinMaxScaler()
-
+# Load model
 model = load_model(file_model, compile=False)
+
+# Count number of files in input directory
+n_files = len(list(dir_input_files.iterdir()))
+
+# Raise error if no files are found
+if n_files == 0:
+    raise ValueError(
+        f"No files found in {dir_input_files}. Please add files to this directory."
+    )
+
+# Iterate of files in input directory
 for file in dir_input_files.iterdir():
     # Skip entries that are not files
     if not file.is_file():
